4db2f3260a616a088d519fcedc5485f32607e85a
Kevin Wittek <kevin.wittek@groovy-coder.com>
2020-02-29 14:54:36 +0100

    Include cropping and scaling of  source image

--- webcam_mirror.sh
+++ webcam_mirror.sh
@@ -10,13 +10,21 @@ then
   exit 1
 fi
 
-device_number=5
+# set device number according to "ls -ltrh /dev/video*" output
+device_number=4
+
 # see https://gstreamer.freedesktop.org/documentation/video4linux2/v4l2src.html?gi-language=c#v4l2src_GstV4l2IOMode
 io_mode=4
+
+# crop source image by this amount of pixels on the top and bottom
+crop_y=200
+crop_x=$(expr 17778 \* $crop_y / 10000) # calculated to keep aspect-ratio
+
 
 if [ "$delay_in_ms" -eq "0" ]; then
     gst-launch-1.0 v4l2src device=/dev/video$device_number io-mode=$io_mode \
     ! video/x-raw, format=UYVY, framerate=60/1, width=1920, height=1080 \
+    ! videobox left=$crop_x right=$crop_x top=$crop_y bottom=$crop_y \
     ! glimagesink sync=false brightness=-0.22 render-rectangle="<0,0,1920,1080>" rotate-method=horizontal-flip 
 else
     delay_in_ns=$(expr 1000000 \* $delay_in_ms)
@@ -24,6 +32,7 @@ else
 
     gst-launch-1.0 v4l2src device=/dev/video$device_number io-mode=$io_mode \
     ! video/x-raw, format=UYVY, framerate=60/1, width=1920, height=1080 \
+    ! videobox left=$crop_x right=$crop_x top=$crop_y bottom=$crop_y \
     ! queue name=q min-threshold-time=$delay_in_ns max-size-buffers=0 max-size-bytes=0 max-size-time=0 \
     ! glimagesink sync=false brightness=-0.22 render-rectangle="<0,0,1920,1080>" rotate-method=horizontal-flip 
 fi
